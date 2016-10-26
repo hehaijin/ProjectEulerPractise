8e3741ad00c3ab6850458d1a40818a422aa18c03
hehaijin <hehaijin1981@gmail.com>
2016-10-26 14:48:52 -0600

    some annotations

--- src/Multiple35.java
+++ src/Multiple35.java
@@ -1,30 +1,45 @@
+/**
+ * 
+ * @author Haijin He
+ *
+ */
 
-public class Multiple35 {
-	
-	public static int sum(int a)
-	{
-		int sum=0;
-		for(int i=1;i<a;i++)
-			if(check35(i)) sum=sum+i;
-		
-		
-		
-		return sum;
-	}
-	
-	
-	public static boolean check35(int a)
-	{
-		return a%5==0|| a%3==0;
 
-	}
-	
+public class Multiple35
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-   
-		System.out.println(sum(1000));
-		
-	}
+  /**
+   * sums all integers from 1 to input a that can be divided by 3 or 5.
+   * @param a
+   * @return
+   */
+  public static int sum(int a)
+  {
+    int sum = 0;
+    for (int i = 1; i < a; i++)
+      if (check35(i))
+        sum = sum + i;
+
+    return sum;
+  }
+
+  /**
+   * check if a int can be divided by 3 or 5
+   * @param a
+   * @return
+   */
+  public static boolean check35(int a)
+  {
+    return a % 5 == 0 || a % 3 == 0;
+
+  }
+
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+
+    System.out.println(sum(1000));
+
+  }
 
 }
